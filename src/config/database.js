f8848b5f3c392a64f05cd1ca132e780507a74be6
funktasthic <ignacioavendanoramirez@gmail.com>
2025-04-01 15:09:44 -0300

    feat: Update database storage

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const db = new Sequelize({
     dialect: "sqlite",
     logging: false,
-    storage: `src/database/${process.env.DB_NAME}.sqlite`,
+    storage: `${process.env.DB_NAME}.sqlite`,
 });
 
 module.exports = db;
