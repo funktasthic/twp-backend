b03b83e43fec013ac8fae9dcef76b03fa913351e
funktasthic <ignacioavendanoramirez@gmail.com>
2025-04-01 12:47:55 -0300

    feat: Add sockets for real time requests

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.server = require("http").createServer(this.app);
+    this.io = require("socket.io")(this.server);
 
     // Paths
     this.paths = {
