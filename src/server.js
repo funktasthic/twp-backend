9f8bcba806ad0e0929d81b287666ca002d2f1f2d
funktasthic <ignacio.avendano@alumnos.ucn.cl>
2025-06-04 17:23:07 -0400

    feat(server): register image on backend

--- src/server.js
+++ src/server.js
@@ -1,21 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const db = require("./config/database");
-const User = require("./models/user");
-const Address = require("./models/address");
-const Role = require("./models/role");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+
+// Models
+const db = require('./config/database');
+const User = require('./models/user');
+const Address = require('./models/address');
+const Role = require('./models/role');
+const Image = require('./models/image');
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-    this.server = require("http").createServer(this.app);
-    this.io = require("socket.io")(this.server);
+    this.server = require('http').createServer(this.app);
+    this.io = require('socket.io')(this.server);
 
     // Paths
     this.paths = {
-      auth: "/api/",
+      default: '/api/',
+      fileUpload: '/api/upload'
     };
 
     // Connect to the database
@@ -39,7 +44,8 @@ class Server {
       await Address.sync({force: false});
       await Role.sync({force: false});
       await User.sync({force: false});
-      console.log("Database connected");
+      await Image.sync({force: false});
+      console.log('Database connected');
     } catch (error) {
       console.log(error);
     }
@@ -47,18 +53,26 @@ class Server {
 
   middlewares() {
     // Morgan
-    this.app.use(morgan("dev"));
+    this.app.use(morgan('dev'));
 
     // Read and parse body
     this.app.use(express.json());
 
-    // CORS
+    // CORS 
     this.app.use(cors());
+
+    // File Upload
+    this.app.use(fileUpload({
+      useTempFiles: true,
+      tempFileDir: '/tmp/',
+      createParentPath: true
+    }))
   }
 
   routes() {
     // Use route paths
-    this.app.use(this.paths.auth, require("./routes/authRoutes"));
+    this.app.use(this.paths.default, require('./routes/authRoutes'));
+    this.app.use(this.paths.fileUpload, require('./routes/fileUploadRoutes'));
   }
 
   listen() {
